331155d90c81f23a2884db4874fbf25285af5271
SebasGuerrag1 <159485822+SebasGuerrag1@users.noreply.github.com>
2024-03-17 22:12:52 -0500

    Update Zona.py

--- gestion/Zona.py
+++ gestion/Zona.py
@@ -1,5 +1,5 @@
 class Zona:
-    def __init__(self,nombre,zoo,animales=None):
+    def __init__(self,nombre=None,zoo=None,animales=None):
         self._nombre= nombre
         self._zoo= zoo
         if animales==None:
